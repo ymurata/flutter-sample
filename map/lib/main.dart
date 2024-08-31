58a8b64611323113e42e663b6c672b9dc9403fca
ymurata <m.yuma0531@gmail.com>
2024-08-31 22:16:03 +0900

    wip

--- map/lib/main.dart
+++ map/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -29,6 +30,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final int numberOfMarker = 30;
+  final int maxLatScale = 14;
+  final int maxLngScale = 8;
+  final int scale = 1000;
+  List<Marker> _markers = [];
+
+  LatLng _createRandomLatLng(LatLng center) {
+    double lat =  math.Random().nextInt(2 * maxLatScale) / scale;
+    double lng =  math.Random().nextInt(2 * maxLngScale) / scale;
+    return new LatLng(center.latitude - (maxLatScale / scale) + lat, center.longitude - (maxLngScale / scale) + lng);
+  }
+
+  void _createMarker(LatLng position) {
+    List<Marker> markers = new List.generate(numberOfMarker, (_) =>
+      Marker(
+        point: _createRandomLatLng(position),
+        child: Icon(
+          Icons.place,
+          color: Colors.blue,
+          size: 40,
+        ),
+      )
+    );
+    setState(() {
+      _markers = markers;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,22 +67,18 @@ class _MyHomePageState extends State<MyHomePage> {
           initialZoom: 15.0,
           maxZoom: 16,
           minZoom: 14,
+          onMapEvent: (MapEvent event) => {
+            if (event is MapEventMoveEnd) {
+              _createMarker(event.camera.center)
+            }
+          },
         ),
         children: [
           TileLayer(
              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           ),
           MarkerLayer(
-            markers: [
-              Marker(
-                point: LatLng(35.681, 139.767),
-                child: Icon(
-                  Icons.place,
-                  color: Colors.red,
-                  size: 40,
-                ),
-              ),
-            ],
+            markers: _markers,
           ),
         ],
       ),
